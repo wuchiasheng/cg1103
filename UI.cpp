379b75b1d6df755eee59673285ce16ba1e170ad7
wuchiasheng <wu_chiasheng@hotmail.com>
2013-02-14 04:55:51 -0800

    Create UI.cpp

---
+++ UI.cpp
@@ -0,0 +1,18 @@
+class UI
+{
+  private:
+          string _Name;
+          string _Category;
+          double _Barcode;
+          double _Price;
+          string _Manufacturer;
+          int    _num_in_stock;
+          int    _num_sold;
+
+ public:
+        void getName()
+        {
+        cout>>"">>endl;
+        }
+};
+
